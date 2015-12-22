a0197000c73b67adb443aa9bc1b2f2f2c82e1bf0
randreucetti <ross.andreucetti@gmail.com>
2015-12-22 17:56:45 +0000

    added player profiles

--- js/ranelagh-roster.js
+++ js/ranelagh-roster.js
@@ -10,8 +10,34 @@ $(document).ready(function() {
 });
 
 function setTabletopVars(data, tabletop) {
-  var years = tabletop.model_names.sort().reverse();
-  for (var i = 0; i < years.length; i++) {
-    
+  var year = getUrlParameter('year');
+  var sheet = tabletop.sheets(year);
+  var html = '';
+  $.each(sheet.all(), function(j, player) {
+    if (j % 3 === 0) {
+      html += '<div class="row">';
+    }
+    html += '<div class="col-sm-4">';
+    html += '<figure class="profile"><img src="http://lorempixel.com/140/140" class="img-circle img-responsive"/><figcaption class="img-circle">12</figcaption></figure>';
+    html += '</div>';
+    if (j % 2 === 3) {
+      html += '</div>';
+    }
+  });
+  $('#roster').append(html);
+}
+
+function getUrlParameter(sParam) {
+  var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+    sURLVariables = sPageURL.split('&'),
+    sParameterName,
+    i;
+
+  for (i = 0; i < sURLVariables.length; i++) {
+    sParameterName = sURLVariables[i].split('=');
+
+    if (sParameterName[0] === sParam) {
+      return sParameterName[1] === undefined ? true : sParameterName[1];
+    }
   }
 }
